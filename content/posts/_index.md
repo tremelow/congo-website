5e20418df37e4016274de069053bedc070a0b49b
Léopold Trémant <29092877+tremelow@users.noreply.github.com>
2024-02-27 11:31:44 +0100

    experimenting post-process TikZ

---
+++ content/posts/_index.md
@@ -0,0 +1,14 @@
+---
+title: "Blog posts"
+description: "My tribulations about maths and maybe other things."
+
+cascade:
+  showEdit: false
+  showAuthor: false
+  showSummary: true
+---
+
+{{< lead >}}
+My tribulations about maths and maybe other things.
+{{< /lead >}}
+
---
+++ public/lib/tikzjax/postproc-tikz-svg.bf455a80ca7a2834a9a859a1f8cea7483585ffffbd7223843fa65c26cd01592d12d7141f04bb2401c21bbf5c30242906fc0876eeca8a99703207991c19de95bb.js
@@ -0,0 +1,14 @@
+var bg-color = var(--tw-prose-pre-bg)
+
+function convertSVG(svg) {
+	return svg.replaceAll(/("#000"|"black")/g, `"currentColor"`)
+              .replaceAll(/("#fff"|"white")/g, `"var(bg-color)"`);
+}
+
+var scripts = document.getElementsByTagName('script');
+var tikzScripts = Array.prototype.slice.call(scripts).filter(
+    (e) => (e.getAttribute('type') === 'image/tikz'));
+
+tikzScripts.map((s) => s.outerHTML = convertSVG(s.innerHTML));
+// should be made cleaner using s.getElementsByTagName("svg")
+// but that doesn't seem to work sometimes...
